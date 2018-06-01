a89aa067d32fd17332748676672f5e93911e6bc9
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-01 18:26:12 +0100

    refactored noteListView, added unit tests for maxLengthCheck

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -1,21 +1,30 @@
 (function(exports) {
 
-  console.log(`%cNoteListView`, `color: GoldenRod`)
+  console.log(`%cNoteListView`, `color: GoldenRod`);
 
   describe("#htmlString")
-  it("converts 1 individual note into html format")
+  it("converts 1 individual note into html format");
     var notes = ["Hi"];
     var List = new NoteListView(notes);
-    assert.isEqual(List.htmlString(), "<ul><li>Note 1: Hi</li></ul>");
+    assert.isEqual(List.htmlString(), `<ul><li><a>href="#note1"</a>Note 1: Hi</li></ul>`);
 
-  it("converts empty notes into html format")
+  it("converts empty notes into html format");
     var notesEmpty = new NoteList().notes;
     var List_empty = new NoteListView(notesEmpty);
-    assert.isEqual(List_empty.htmlString(), "<ul></ul>");
+    assert.isEqual(List_empty.htmlString(), `<ul></ul>`);
 
-    it("will limit text shown to 20 chars")
-    var notes = ["123456789012345iiiiii678901"];
+    it("will limit text shown to 20 chars");
+    var notes = ["123456789012345678901"];
     var List2 = new NoteListView(notes);
-    assert.isEqual(List2.htmlString(), "<ul><li>Note 1: 12345678901234567890...</li></ul>")
+    assert.isEqual(List2.htmlString(), `<ul><li><a>href="#note1"</a>Note 1: 12345678901234567890...</li></ul>`);
+
+  describe("#maxLengthCheck");
+  it("return false when note is smaller than max length");
+    var List = new NoteListView();
+    note = "12345";
+    assert.isEqual(List.maxLengthCheck(note), false);
+  it("returns true when note is greater than max length");
+    var largeNote = "123456789012345678901"
+    assert.isEqual(List.maxLengthCheck(largeNote), true);
 
 })(this);
--- src/noteListView.js
+++ src/noteListView.js
@@ -1,16 +1,30 @@
 (function(exports){
   function NoteListView(notes) {
+    var note;
+    this.maxLength = 20;
+    this.htmlListArray = [];
     this.notes = notes || new NoteList().notes;
   }
 
+  // converts individual notes to html
   NoteListView.prototype.htmlString = function() {
-    var htmlList = [];
+    var current = this; // makes 'this' part of the lexical context of the function, aka function knows what this is
 
-    this.notes.forEach(function(note, index) {
-      htmlList.push(`<li>Note ${index + 1}: ${note}</li>`);
+    current.notes.forEach(function(note, index) {
+      if ( current.maxLengthCheck(note) ) {
+        note = note.slice( 0, current.maxLength )
+        current.htmlListArray.push(`<li><a>href="#note${index + 1}"</a>Note ${index + 1}: ${note}...</li>`);
+      } else {
+        current.htmlListArray.push(`<li><a>href="#note${index + 1}"</a>Note ${index + 1}: ${note}</li>`);
+      }
     });
-    return (`<ul>${htmlList.join("")}</ul>`);
+    return (`<ul>${current.htmlListArray.join("")}</ul>`);
   };
+
+  // checks notes length
+  NoteListView.prototype.maxLengthCheck = function(note) {
+    return note.length > this.maxLength;
+  }
 
   exports.NoteListView = NoteListView;
 
